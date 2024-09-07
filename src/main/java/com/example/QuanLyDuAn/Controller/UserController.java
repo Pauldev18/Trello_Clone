301616b8967246ba796fc72485d6d2632097ec90
PaulDev <vuquanganh1802@gmail.com>
2024-09-07 15:26:46 +0700

    ok

--- src/main/java/com/example/QuanLyDuAn/Controller/UserController.java
+++ src/main/java/com/example/QuanLyDuAn/Controller/UserController.java
@@ -7,7 +7,7 @@ import com.example.QuanLyDuAn.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
